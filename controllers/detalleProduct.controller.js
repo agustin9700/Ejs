d2c8b865e458fa96e82f9432b80b2cfce123fc3f
agustin9700 <ryuk10xd@gmail.com>
2024-02-20 01:12:07 -0300

    agregando vista de detalle

--- controllers/detalleProduct.controller.js
+++ controllers/detalleProduct.controller.js
@@ -1,35 +1,39 @@
 module.exports = function (req, res) {
     const infoProduct = [
-        {
-            id: 1,
-            nombre: "Carpaccio Fresco",
-            detalle: "Entrada Carpaccio de salmón con cítricos",
-            precio: 1000
-        },
-        {
-            id: 2,
-            nombre: "Risotto de Berenjena",
-            detalle: "Risotto de berenjena y queso de cabra",
-            precio: 2000
-        },
-        {
-            id: 3,
-            nombre: "Mousse de Arroz",
-            detalle: "Mousse de arroz con leche y aroma de azahar",
-            precio: 3000
-        },
-        {
-            id: 4,
-            nombre: "Espárragos Blancos",
-            detalle: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
-            precio: 4000
-        }
-    ];
+    {
+        id: 1,
+        nombre: "Carpaccio Fresco",
+        detalle: "Entrada Carpaccio de salmón con cítricos",
+        precio: 1000,
+        imagen: "/images/Carpaccio-de-salmon.jpg"  
+    },
+    {
+        id: 2,
+        nombre: "Risotto de Berenjena",
+        detalle: "Risotto de berenjena y queso de cabra",
+        precio: 2000,
+        imagen: "/images/Risotto-berenjena-queso-cabra.jpg"  
+    },
+    {
+        id: 3,
+        nombre: "Mousse de Arroz",
+        detalle: "Mousse de arroz con leche y aroma de azahar",
+        precio: 3000,
+        imagen: "/images/Mousse-de-arroz-con-leche.jpg"  
+    },
+    {
+        id: 4,
+        nombre: "Espárragos Blancos",
+        detalle: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+        precio: 4000,
+        imagen: "/images/esparragos.jpg"  
+    }
+];
     const idMenu = req.params.id;
     const productoSeleccionado = infoProduct.find(function (producto) {
         return producto.id === parseInt(idMenu);
     });
 
     // Pasa los datos del producto seleccionado a la vista
-    res.render("detalleMenu", { producto: productoSeleccionado });
+    res.render("detalleMenu", { "productoSeleccionado": productoSeleccionado });
 };
